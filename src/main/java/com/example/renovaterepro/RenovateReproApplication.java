fde39d3b7a3f3b60e9de824abf63e1ee5d9fe60b
Colin Plank <cmplank@liberty.edu>
2022-03-18 18:03:03 -0400

    Initial commit

---
+++ src/main/java/com/example/renovaterepro/RenovateReproApplication.java
@@ -0,0 +1,13 @@
+package com.example.renovaterepro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenovateReproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenovateReproApplication.class, args);
+	}
+
+}
